f81eb7bddbbf74146fcb1fadb65ad63121ccfb93
Stijn <stijn.lafontaine@ziggo.nl>
2023-10-23 15:49:57 +0200

    adding image sorting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 from manual_bbox_annotation import image_bounding_box_annotation, key_checker
 from bbox_class_annotation import Bounding_box_classification_annotation_tool
 
@@ -24,6 +25,7 @@ def main():
 def main2():
     directory_path = "captured_images"
     images = []
+    values = []
     PATH_TO_MODEL = 'product_detection_weights.pt'
 
 
@@ -33,18 +35,20 @@ def main2():
     for filename in os.listdir(directory_path):
         f = os.path.join(directory_path, filename)
         # checking if it is a file
-        if os.path.isfile(f) and (f.endswith('.jpg') or f.endswith('.png')):
+        if os.path.isfile(f) and (f.endswith('.jpg')):
             images.append(f)
+            values.append(int(f.replace('.jpg','')))
 
+    images = [i[1] for i in sorted(zip(values, images))]
     num = 0 # resume annotation
     
     for i in range(len(images)):
         print(i+num) # +1 due to classes.txt
         image = images[i+num+1]
-        print(image)
-        name = str(i+num) + "_rgb"
+        name = str(i+num)
         new_annotation = Bounding_box_classification_annotation_tool(PATH_TO_MODEL, image, name, classes)
         new_annotation.annotate()
+        cv2.imwrite(name, cv2.imread(image))
 
 
 if __name__ == '__main__':
