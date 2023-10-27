343c1d589d94bf3c6d8a1c9f5d9283d5fa4cf464
Stijn <stijn.lafontaine@ziggo.nl>
2023-10-27 09:40:55 +0200

    cleaned main.py

--- main.py
+++ main.py
@@ -3,26 +3,9 @@ import cv2
 from manual_bbox_annotation import image_bounding_box_annotation, key_checker
 from bbox_class_annotation import Bounding_box_classification_annotation_tool
 
+
+
 def main():
-    directory_path = "raw_images"
-    images = []
-
-
-    with open(os.path.join(directory_path, 'classes.txt')) as f:
-        classes = f.readlines()
-    for filename in os.listdir(directory_path):
-        f = os.path.join(directory_path, filename)
-        # checking if it is a file
-        if os.path.isfile(f):
-            images.append(f)
-
-    for i, image in enumerate(images):
-        name = str(i) + "_rgb"
-        new_annotation = image_bounding_box_annotation(image, name, classes)
-        new_annotation.annotate()
-    
-
-def main2():
     directory_path = "captured_images/data"
     classes_path = "captured_images"
     images = []
@@ -46,7 +29,6 @@ def main2():
     num = len(processed_images) # resume annotation
     
     for i in range(len(images)):
-        print(i+num) # +1 due to classes.txt
         image = images[i+num]
         name = str(i+num)
         new_annotation = Bounding_box_classification_annotation_tool(PATH_TO_MODEL, image, name, classes)
@@ -55,4 +37,4 @@ def main2():
 
 
 if __name__ == '__main__':
-    main2()
+    main()
